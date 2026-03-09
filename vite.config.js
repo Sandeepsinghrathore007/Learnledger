702586636afd135581db07273772130d646185aa
Sandeep Rathore <sandeepsinghrathorerajpoot@gmail.com>
2026-03-09 18:46:13 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Learnledger/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,4 +12,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
